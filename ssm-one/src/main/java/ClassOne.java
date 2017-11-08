94cf4dfd16236c04996b1ff1b4e2746c592f3930
shilei <shilei@hundsun.com>
2017-11-08 10:29:44 +0800

    多模块提交测试
    多模块提交测试

---
+++ ssm-one/src/main/java/ClassOne.java
@@ -0,0 +1,20 @@
+/********************************************
+ * 文件名称: ClassOne
+ * 软件版权: 恒生电子股份有限公司
+ * 功能说明: 
+ * 系统版本: 1.0
+ * 开发人员: shi-lei
+ * 开发时间: 2017-11-08 10:27
+ * 修改记录: 版本信息  修改标签  修改人员  修改说明  修改单编号
+ *********************************************/
+public class ClassOne {
+    private String  one;
+
+    public String getOne() {
+        return one;
+    }
+
+    public void setOne(String one) {
+        this.one = one;
+    }
+}
---
+++ ssm-two/src/main/java/ClassTwo.java
@@ -0,0 +1,20 @@
+/********************************************
+ * 文件名称: ClassTwo
+ * 软件版权: 恒生电子股份有限公司
+ * 功能说明: 
+ * 系统版本: 1.0
+ * 开发人员: shi-lei
+ * 开发时间: 2017-11-08 10:27
+ * 修改记录: 版本信息  修改标签  修改人员  修改说明  修改单编号
+ *********************************************/
+public class ClassTwo {
+    private String two;
+
+    public String getTwo() {
+        return two;
+    }
+
+    public void setTwo(String two) {
+        this.two = two;
+    }
+}
